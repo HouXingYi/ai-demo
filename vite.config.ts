71396bf2ee93c9257674bacaeb9055f052316d93
HXY\10260 <1026043046@qq.com>
2025-08-25 09:59:08 +0800

    dev: 允许外部访问

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // 允许外部访问
     open: true, // 自动打开浏览器
     port: 5173, // 指定端口
   }
